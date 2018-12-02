85243270ac9e3b5a4336d422201467196c56896f
haymard <haymard@HAYMARD-AR.ar.oracle.com>
2018-12-02 10:10:21 -0300

    ..

--- howto-service/registry.js
+++ howto-service/registry.js
@@ -3,6 +3,7 @@
 module.exports = {
   components: {
    'ar.hacerPedido' : require('./chatbot/hacerPedido'),
-   'ar.cancelarPedido' : require('./chatbot/cancelarPedido') 
+   'ar.cancelarPedido' : require('./chatbot/cancelarPedido'),
+   'ar.conversador' : require('./chatbot/conversador')
   }
 };
